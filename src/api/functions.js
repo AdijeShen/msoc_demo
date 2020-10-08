d476482e474203a899f88a0c8e64d207d7b3bb93
adijeshen <1534656015@qq.com>
2020-10-08 21:30:45 +0800

    first commit

---
+++ src/api/functions.js
@@ -0,0 +1,69 @@
+import requester from "@/utils/network"
+
+export function addfunction(data) {
+  return requester({
+    url: '/demo/addFunction',
+    method: 'post',
+    params: data
+  })
+}
+
+export function findAllFunctions(data) {
+  return requester({
+    url: '/demo/findAllFunctions',
+    method: 'post',
+    params: data
+  })
+}
+export function loadfunctiondetail(data) {
+  return requester({
+    url: '/demo/findOneFunctionById',
+    method: 'post',
+    params: data
+  })
+}
+export function updatefunctionparams(data) {
+  return requester({
+    url: '/demo/updateFunctionParams',
+    method: 'post',
+    params: data
+  })
+}
+export function getresult(data) {
+  return requester({
+    url: '/demo/getResult',
+    method: 'post',
+    params: data
+  })
+}
+
+export function getCResult(data) {
+  return requester({
+    url: '/demo/getCipherResult',
+    method: 'post',
+    params: data
+  })
+}
+
+export function getUserCipherText(data) {
+  return requester({
+    url: '/demo/getUserCipherText',
+    method: 'post',
+    params: data
+  })
+}
+
+export function gettime(data) {
+  return requester({
+    url: '/demo/getTime',
+    method: 'post'
+  })
+}
+
+export function getcspciphertext(data) {
+  return requester({
+    url: '/demo/getCspCipherText',
+    method: 'post',
+    params: data
+  })
+}
